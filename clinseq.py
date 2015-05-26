8256fdf473960479fcce46ae6b366d0033dd514f
Alex H. Wagner <awagner@genome.wustl.edu>
2015-05-26 16:47:54 -0500

    Add support for model groups. Fix clinseq to differentiate model from model group.

---
+++ clinseq.py
@@ -0,0 +1,28 @@
+__author__ = 'Alex H Wagner'
+
+from model import GMSModel, GMSModelGroup
+
+class ClinSeqModel(GMSModel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gms_type = 'clin-seq'
+        self.show_values = {'model_id': 'id',
+                            'name': 'name',
+                            'subject_common_name': 'subject.common_name',
+                            'wgs_id': 'wgs_model.id',
+                            'exome_id': 'exome_model.id',
+                            'tumor_rnaseq': 'tumor_rnaseq_model.id',
+                            'normal_rnaseq': 'normal_rnaseq_model.id'}
+        self.filter_values = {'model_groups.id': self.model_id}
+
+
+class ClinSeqModelGroup(ClinSeqModel, GMSModelGroup):
+
+    pass
+
+if __name__ == '__main__':
+    c = ClinSeqModelGroup('d7369c20395742568c79bdf0999d1f30')
+    c.update()
+    rnaseq = [x.tumor_rnaseq for x in c.models if hasattr(x, 'tumor_rnaseq')]
+    print(len(c.models), len(rnaseq))
--- model.py
+++ model.py
@@ -23,14 +23,11 @@ class GMSModel:
         fd = self.filter_values
         f_keys = sorted(fd)
         f_call = ','.join(['{0}={1}'.format(x, fd[x]) for x in f_keys])
-        c = 'genome model {0} list --noheaders --filter {1} --show {2}'\
-            .format(self.gms_type, f_call, v_call)
         c = 'genome model {0} list --noheaders'.format(self.gms_type)
         if f_call:
             c += ' --filter {0}'.format(f_call)
         if v_call:
             c += ' --show {0}'.format(v_call)
-        print(c)
         r = self.linus.command(c, timeout=15)
         if raw:
             return(r)
@@ -42,3 +39,58 @@ class GMSModel:
             if d[k] == '<NULL>':
                 continue
             setattr(self, k, d[k])
+
+
+class GMSModelGroup(GMSModel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.models = []
+
+    def update(self, raw=False):
+        """raw=False processes attributes extracted from call response. raw=True returns the call response instead."""
+        r = super().update(raw=True)
+        if raw:
+            return(r)
+        keys = sorted(self.show_values)
+        for line in r.split('\n'):
+            d = dict(zip(keys, line.split()))
+            d = {k: v for k, v in d.items() if v != '<NULL>'}
+            primary_parent_class = self.__class__.__bases__[0]
+            self.models.append(primary_parent_class(**d))
+
+    def __len__(self):
+        return len(self.models)
+
+    # def select(self, **kw):
+    #     keys = sorted(self.show_values)
+    #     result = []
+    #     for k in kw:
+    #         if k not in keys:
+    #             raise KeyError('Invalid key {0}. See self.show_values for keys.'.format(k))
+    #         v = kw[k]
+    #         try:
+    #             d = getattr(self, k)
+    #         except AttributeError:
+    #             if v is not False:
+    #                 return None
+    #             else:
+    #                 result.append(self.model_ids)
+    #                 continue
+    #         if v is True:
+    #             result.append(set(d))
+    #         elif v is False:
+    #             result.append(self.model_ids - set(d))
+    #         else:
+    #             s = set()
+    #             for dk in d:
+    #                 if d[dk] == v:
+    #                     s.add(dk)
+    #             result.append(s)
+    #     if result:
+    #         s = result.pop()
+    #         while result:
+    #             s &= result.pop()
+    #         if s:
+    #             return s
+    #     return None
